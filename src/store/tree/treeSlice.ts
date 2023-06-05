7470db27c76a6d6d75eaee90b9781fa6c97c9d23
alyoneek <alyonta03@mail.ru>
2023-06-05 15:58:54 +0700

    (store) Change crud operations for nested nodes

--- src/store/tree/treeSlice.ts
+++ src/store/tree/treeSlice.ts
@@ -8,23 +8,47 @@ export interface INode {
 }
 
 interface ITreeState {
-  nodes: INode[];
+  nodes: INode;
   choosenNode: number | null;
 }
 
 const initialState: ITreeState = {
-  nodes: [],
+  nodes: {
+    id: 0,
+    name: "",
+    children: [],
+  },
   choosenNode: null,
 };
 
-const a = (addToNode: INode, nodeId: number, newNode: INode): INode => {
-  if (addToNode.id === nodeId) {
-    addToNode.children.push(newNode);
-    return addToNode;
+const findNode = (tree: INode, nodeId: number): INode | null => {
+  if (nodeId === 0 || tree.id === nodeId) {
+    return tree;
   }
 
-  const newChildren = addToNode.children.map((node) => a(node, nodeId, newNode));
-  return { ...addToNode, children: newChildren };
+  let node = null;
+
+  for (let i = 0; i < tree.children.length; i++) {
+    node = findNode(tree.children[i], nodeId);
+    if (node) break;
+  }
+
+  return node;
+};
+
+const deleteNode = (tree: INode, nodeId: number): INode | null => {
+  let node = null;
+
+  for (let i = 0; i < tree.children.length; i++) {
+    if (tree.children[i].id === nodeId) {
+      node = tree.children.splice(i, 1)[0];
+      break;
+    }
+    node = deleteNode(tree.children[i], nodeId);
+    if (node) break;
+  }
+
+  return node;
 };
 
 export const treeSlice = createSlice({
@@ -38,24 +62,21 @@ export const treeSlice = createSlice({
         children: [],
       };
 
-      if (action.payload.id === 0) {
-        state.nodes.push(newNode);
-      } else {
-        state.nodes = state.nodes.map((node) => a(node, action.payload.id, newNode));
+      const node = findNode(state.nodes, action.payload.id);
+      if (node) {
+        node.children.push(newNode);
       }
     },
     editNode: (state, action: PayloadAction<Omit<INode, "children">>) => {
-      const node = state.nodes.find((node) => node.id === action.payload.id);
+      const node = findNode(state.nodes, action.payload.id);
       if (node) {
         node.name = action.payload.name;
       }
     },
     deleteNode: (state, action: PayloadAction<number>) => {
-      state.nodes = state.nodes.filter((node) => node.id !== action.payload);
+      deleteNode(state.nodes, action.payload);
     },
-    clearTree: (state) => {
-      state.nodes = [];
-    },
+    clearTree: () => initialState,
     chooseNode: (state, action: PayloadAction<number | null>) => {
       state.choosenNode = action.payload;
     },
